71cb6c5b5111c3972475ade9cfbb2b226a60370b
NzSN <nzsn0101@gmail.com>
2025-02-14 12:02:05 +0800

    U

---
+++ ast/ast_generator.h
@@ -0,0 +1,14 @@
+#ifndef KALEIS_AST_AST_GENERATOR_H_
+#define KALEIS_AST_AST_GENERATOR_H_
+
+namespace Kaleis {
+namespace AST {
+
+class ASTGenerator {
+
+};
+
+} // AST
+} // Kaleis
+
+#endif /* KALEIS_AST_AST_GENERATOR_H_ */
